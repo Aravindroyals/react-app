b873bc3c08a83973d3e64072410b70ebcab27b2c
Aravindroyals <134695735+Aravindroyals@users.noreply.github.com>
2023-08-02 15:51:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+  res.send('Hello, Cloud Run!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
